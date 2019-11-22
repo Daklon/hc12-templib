77ec9f4ce5369d7af1764772f47c55529158bbea
Javier Herrera <alu0100830972@ull.edu.es>
2019-11-22 13:02:33 +0000

    implemented future_function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,11 @@
 
 //declaramos variable global, un contador para aumentar el tiempo que puede contar el timer
 uint16_t expanded_timer = 0;
+uint16_t expanded_programmed_timer = 0;
+uint16_t iteration_triger = 0;
 uint8_t preescale = 0;
+
+void *future_function;
 
 //función a la que se llama con una interrupción
 //cada vez que se desborda el timer
@@ -14,6 +18,21 @@ uint8_t preescale = 0;
 void __attribute__((interrupt)) vi_tov(void){
     expanded_timer++; //incrementamos expanded timer
     _io_ports[M6812_TFLG2] = M6812B_TOF;//bajamos el flag que disparó la interrupción
+}
+
+//función a la que se llama con una interrupción
+//cuando el timer alcanza el valor programado
+void __attribute__((interrupt)) vi_ioc0(void){
+    _io_ports[M6812_TFLG1] = M6812B_C1F; //bajamos el flag que disparó la interrupción
+    //compruebo si es la última iteración
+    if (expanded_programmed_timer == 0){
+        //desactivar interrupción definitivamente
+        _io_ports[M6812_TMSK1] &= ~M6812B_C1I;
+        //ejecutamos función
+        future_function();
+    } else{//se ha disparado porque se tiene que ejecutar la func
+        expanded_programmed_timer--;
+    }
 }
 
 //Establece el preescaler
@@ -67,10 +86,20 @@ void delayms(uint32_t time){
 	} while (wait_start <= time);
 }
 
-//Ejecuta la función dentro de x milisegundos TODO
+//Ejecuta la función dentro de x microsegundos TODO
 void future_f(void (*f), uint32_t time){
-    
+    expanded_programmed_timer = time >> 16;
+    future_function = f;
+    //Establecemos el modo de comparador a salida(OC)
+    _io_ports[M6812_TIOS] |= M6812B_IOS1;
+    //Machacamos el valor
+	_io_ports[M6812_TFLG1] = M6812B_C1F;
+	//Establecemos el valor del tcnt
+    _IO_PORTS_W(M6812_TC1) = _IO_PORTS_W(M6812_TCNT) + (uint16_t) time; //al hacer cast debería descartar la parte mas significativa
+	//habilitamos interrupciones para el OC
+	_io_ports[M6812_TMSK1] |= M6812B_C1I;
 }
+
 
 //Ejecuta la función cada x milisegundos, de forma periódica TODO
 void periodic_f(void (*f), uint32_t time){
